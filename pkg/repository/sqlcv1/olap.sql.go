cd97d0a9f8259ef92fc9cf8fca3e33f99ce40b2f
mrkaye97 <mrkaye97@gmail.com>
2026-04-29 11:06:55 -0400

    chore: gen

--- pkg/repository/sqlcv1/olap.sql.go
+++ pkg/repository/sqlcv1/olap.sql.go
@@ -4122,7 +4122,7 @@ WITH inputs AS (
         UNNEST($5::UUID[]) AS worker_id,
         UNNEST($6::int[]) AS retry_count
 ), locked_tasks AS (
-    SELECT tenant_id, id, inserted_at, external_id, queue, action_id, step_id, workflow_id, workflow_version_id, workflow_run_id, schedule_timeout, step_timeout, priority, sticky, desired_worker_id, display_name, input, additional_metadata, readable_status, latest_retry_count, latest_worker_id, dag_id, dag_inserted_at, parent_task_external_id
+    SELECT tenant_id, id, inserted_at, external_id, queue, action_id, step_id, workflow_id, workflow_version_id, workflow_run_id, schedule_timeout, step_timeout, priority, sticky, desired_worker_id, display_name, input, additional_metadata, readable_status, latest_retry_count, latest_worker_id, dag_id, dag_inserted_at, parent_task_external_id, is_durable
     FROM v1_tasks_olap
     WHERE (inserted_at, id, tenant_id) IN (
         SELECT task_inserted_at, task_id, tenant_id
